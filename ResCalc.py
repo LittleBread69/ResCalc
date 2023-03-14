c346f9b19c6e411140eeb018fb4772fb1c9f6975
LittleBread69 <111202841+LittleBread69@users.noreply.github.com>
2023-03-14 08:55:30 +0100

    Update ResCalc.py

--- ResCalc.py
+++ ResCalc.py
@@ -6,14 +6,12 @@ def ResCalc(arn: int, ard: int, minHeight: int, maxHeight: int):
     retpx = []
     while n <= maPx:
         for i in range(miPx, maPx):
-            if(i % 2 == 1):
+            if(i % 2 != 0):
                 n += 1
                 pass
             elif (i * arn) % ard == 0 and ((i * arn) % ard) / 2 == 0:
                 n += 1
                 retpx.append([i, int(i*(arn/ard))])
-            else:
-                n += 1
     return retpx
 
 def ResCalc4(arn: int, ard: int, minHeight: int, maxHeight: int):
